dcda7e3eb9e308bec8107c60b3c0724e7b00396f
md mahabubur rahman <md9.mahabubur@gmail.com>
2022-08-12 11:50:15 +0800

    rest api swagger added for non authorized & authorized functions

--- app/Http/Controllers/Api/PassportAuthController.php
+++ app/Http/Controllers/Api/PassportAuthController.php
@@ -9,8 +9,57 @@ use Validator;
 class PassportAuthController extends Controller
 {
 	
-    /**
-     * Registration Req
+    
+	/**
+     * Create User
+     * @OA\POST (
+     *     path="/api/register",
+     *     tags={"UnAuthorize"},
+	 *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+	 * 			   @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="string"
+     *                      ),
+	 *                      @OA\Property(
+     *                          property="password",
+     *                          type="string"
+     *                      ),
+	 *                      @OA\Property(
+     *                          property="c_password",
+     *                          type="string"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "name":"example name",
+     *                     "email":"md9.mahabubur@example.com",
+     *                     "password":"12345678",
+     *                     "c_password":"12345678"
+     *                }
+     *             )
+	 *		   )
+	 *		),
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *				@OA\Property(
+     *                 type="string",
+     *                 property="token",
+	 *				   example="some token value"
+	 *				)
+    
+     *         )
+     *     )
+     * )
      */
     public function register(Request $request)
     {
@@ -33,8 +82,47 @@ class PassportAuthController extends Controller
         return response()->json(['token' => $token], 200);
     }
   
-    /**
-     * Login Req
+    
+	/**
+     * Login to system
+     * @OA\POST (
+     *     path="/api/login",
+     *     tags={"UnAuthorize"},
+	 *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+	 * 			   @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="string"
+     *                      ),
+	 *                      @OA\Property(
+     *                          property="password",
+     *                          type="string"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "email":"md9.mahabubur@example.com",
+     *                     "password":"enter password"
+     *                }
+     *             )
+	 *		   )
+	 *		),
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+	 *				@OA\Property(
+     *                 type="string",
+     *                 property="token",
+	 *				   example="some token value"
+	 *				)
+    
+     *         )
+     *     )
+     * )
      */
     public function login(Request $request)
     {
@@ -50,10 +138,57 @@ class PassportAuthController extends Controller
             return response()->json(['error' => 'Unauthorised'], 401);
         }
     }
- 
+	/**
+     * Show User Details
+     * @OA\GET (
+     *     path="/api/get-user",
+     *     tags={"Authorize"},
+	 *     security={{"bearer_token":{}}}, 
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *               @OA\Property(
+     *                     type="object",
+	 *                     property="user",
+	 *					 	@OA\Property(
+     *                         property="id",
+     *                         type="number",
+     *                         example=1
+     *                     ),
+     *                     @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="example name"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="email",
+     *                         type="string",
+     *                         example="test@asb.cpm"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="detail",
+     *                         type="string",
+     *                         example="example detail"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="updated_at",
+     *                         type="string",
+     *                         example="2021-12-11T09:25:53.000000Z"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="created_at",
+     *                         type="string",
+     *                         example="2021-12-11T09:25:53.000000Z"
+     *                     )
+     *                 )
+    
+     *         )
+     *     )
+     * )
+     */
     public function userInfo() 
     {
- 
      $user = auth()->user();
       
      return response()->json(['user' => $user], 200);
--- routes/api.php
+++ routes/api.php
@@ -28,5 +28,4 @@ Route::post('login', [PassportAuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::get('get-user', [PassportAuthController::class, 'userInfo']);
 	Route::resource('doctors', DoctorController::class);
- 
 });
